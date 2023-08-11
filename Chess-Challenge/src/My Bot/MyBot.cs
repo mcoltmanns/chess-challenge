adeae9b589b80cf09f464d372a9db53f065c0288
Max Oltmanns <max.oltmanns@uni-konstanz.de>
2023-08-11 16:29:47 -0400

    rewrote search to use a-b pruning strategy implemented dynamic search depths based on time taken for last move tweaked evaluation weights a bit

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -12,13 +12,16 @@ public class MyBot : IChessBot
         {PieceType.Bishop, 3f},
         {PieceType.Rook, 5f},
         {PieceType.Queen, 9f},
-        {PieceType.King, 10f}
+        {PieceType.King, 200f}
     };
 
-    static Dictionary<ulong, Tuple<float, float>> stateInfo = new Dictionary<ulong, Tuple<float, float>>();
+    static Dictionary<ulong, float> stateInfo = new Dictionary<ulong, float>();
+
+    int searchDepth = 2;
 
     public Move Think(Board board, Timer timer)
     {
+        Console.WriteLine("searching to a depth of " + searchDepth + "...");
         Move[] allMoves = board.GetLegalMoves();
         bool color = board.IsWhiteToMove;
 
@@ -26,31 +29,25 @@ public class MyBot : IChessBot
         Random rng = new();
         Move moveToPlay = allMoves[rng.Next(allMoves.Length)];
 
-        // deep searches are less important with fewer pieces on the board
-        int searchDepth = 2; // actually deep searches might be better towards the end - current algorithm tends to get into loops
-        // or somehow need to become more aggressive towards the end
-        // when is endgame? how to be more aggressive? look for checks/mates and always play them
-
-        float smallestDiff = float.PositiveInfinity;
         float bestOutcome = float.NegativeInfinity;
-        float worstOutcome = float.PositiveInfinity;
         foreach(Move m in allMoves){
             board.MakeMove(m);
-            Console.Write("searching to a depth of " + searchDepth + "... ");
-            float best = FindBestOutcome(board, searchDepth, color);
-            if(best >= bestOutcome){
-                float worst = FindWorstOutcome(board, searchDepth, color);
-                float diff = best - worst;
-                Console.WriteLine("doing " + m.ToString() + " has a score difference of " + best + " - " + worst + " = " + diff);
-                if(diff <= smallestDiff){ // want to minimize difference, and maximize best outcome
-                    smallestDiff = diff;
-                    bestOutcome = best;
-                    worstOutcome = worst;
-                    moveToPlay = m;
-                }
+            float alphaBeta = AlphaBeta(board, searchDepth, float.NegativeInfinity, float.PositiveInfinity, false, color);
+            if(alphaBeta > bestOutcome){
+                bestOutcome = alphaBeta;
+                moveToPlay = m;
             }
             board.UndoMove(m);
         }
+
+        // deep searches are less important with fewer pieces on the board
+        // usually there are 40 moves per game, so should take about 1.5 sec per move
+        if(timer.MillisecondsElapsedThisTurn < 1250) searchDepth ++; // if we were fast, go deeper next round - constant is arbitrary, tweak as needed
+        else searchDepth --; // otherwise go shallower
+
+        // i think we need some more heuristics
+
+        Console.WriteLine("found best move " + bestOutcome + " in " + timer.MillisecondsElapsedThisTurn + "ms");
 
         return moveToPlay;
     }
@@ -69,7 +66,7 @@ public class MyBot : IChessBot
             + pieceValueLookup[PieceType.Knight] * (pieces[2].Count - pieces[8].Count)
             + pieceValueLookup[PieceType.Bishop] * (pieces[1].Count - pieces[7].Count)
             + pieceValueLookup[PieceType.Pawn] * (pieces[0].Count - pieces[6].Count);
-        score += material * 1.25f; // weight all material
+        score += material * 1f; // weight all material
 
         // consider mobility
         // figure out which squares each side can move to
@@ -122,46 +119,51 @@ public class MyBot : IChessBot
                 }
             }
         }
-        score += (BitboardHelper.GetNumberOfSetBits(whiteMovesBb) - BitboardHelper.GetNumberOfSetBits(blackMovesBb)) * 0.2f; // weighted mobility addition - mobility is good to develop early
+        score += (BitboardHelper.GetNumberOfSetBits(whiteMovesBb) - BitboardHelper.GetNumberOfSetBits(blackMovesBb)) * 0.1f; // weighted mobility addition - mobility is good to develop early
 
         // capture potential
         float captureScore = 0f;
         foreach(Move m in board.GetLegalMoves(true)){
             captureScore += pieceValueLookup[m.CapturePieceType];
         }
-        score += (board.IsWhiteToMove ? captureScore : -captureScore) * 1f;// + (board.PlyCount * 0.1f); // captures are more important later on
+        score += (board.IsWhiteToMove ? captureScore : -captureScore) * 1.5f;// + (board.PlyCount * 0.1f); // captures are more important later on
+
+        // check/mate
+        if(board.IsInCheckmate()){
+            score += board.IsWhiteToMove ? float.NegativeInfinity : float.PositiveInfinity;
+        }
 
         return perspective ? score : -score; // positive good, negative bad!
     }
 
-    // iterative versions?
-    float FindBestOutcome(Board board, int currentDepth, bool perspective){
-        float best = float.NegativeInfinity;
-        if(currentDepth == 0) return Evaluate(board, perspective);
-        foreach(Move move in board.GetLegalMoves()){
-            board.MakeMove(move); // can't be sure what move the opponent might make, so better to check all of them
-            ulong key = board.ZobristKey;
-            float score;
-            if(stateInfo.ContainsKey(key)) score = stateInfo[key].Item1;
-            else score = FindBestOutcome(board, currentDepth - 1, perspective);
-            board.UndoMove(move);
-            if(score > best) best = score;
+    // iterative version?
+    float AlphaBeta(Board board, int depth, float a, float b, bool isMaximizing, bool perspective){
+        Move[] moves = board.GetLegalMoves();
+        if(depth == 0 || moves.Length == 0){
+            return Evaluate(board, perspective);
         }
-        return best;
-    }
-
-    float FindWorstOutcome(Board board, int currentDepth, bool perspective){
-        if(currentDepth == 0) return Evaluate(board, perspective);
-        float worst = float.PositiveInfinity;
-        foreach(Move move in board.GetLegalMoves()){
-            board.MakeMove(move);
-            ulong key = board.ZobristKey;
-            float score;
-            if(stateInfo.ContainsKey(key)) score = stateInfo[key].Item2;
-            else score = FindWorstOutcome(board, currentDepth - 1, perspective);
-            board.UndoMove(move);
-            if(score < worst) worst = score;
+        float value;
+        if(isMaximizing){
+            value = float.NegativeInfinity;
+            foreach(Move move in moves){
+                board.MakeMove(move);
+                value = MathF.Max(value, AlphaBeta(board, depth - 1, a, b, false, perspective)); 
+                board.UndoMove(move);
+                if(value > b) break;
+                a = MathF.Max(a, value);
+            }
+            return value;
         }
-        return worst;
+        else{
+            value = float.PositiveInfinity;
+            foreach(Move move in moves){
+                board.MakeMove(move);
+                value = MathF.Min(value, AlphaBeta(board, depth - 1, a, b, true, perspective));
+                board.UndoMove(move);
+                if(value < a) break;
+                b = MathF.Min(b, value);
+            }
+            return value;
+        }
     }
 }
