f92207da6a931aa5c155b62976457a0962879f61
Max Oltmanns <max.oltmanns@uni-konstanz.de>
2023-08-15 10:39:16 -0400

    fixed bug when inserting keys into transposition table

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -26,7 +26,7 @@ public class MyBot : IChessBot
         double bestOutcome = double.NegativeInfinity;
         foreach(Move m in moves){
             board.MakeMove(m);
-            double alphaBeta = AlphaBeta(board, 5, double.NegativeInfinity, double.PositiveInfinity, false, !board.IsWhiteToMove); // 3 seems to be the maximum reasonable search depth
+            double alphaBeta = AlphaBeta(board, 4, double.NegativeInfinity, double.PositiveInfinity, false, !board.IsWhiteToMove); // 3 seems to be the maximum reasonable search depth
             // focus on either improving the eval, or speeding up search somehow to get more depth out of it
             if(alphaBeta > bestOutcome){
                 bestOutcome = alphaBeta;
@@ -50,6 +50,15 @@ public class MyBot : IChessBot
             + pieceValueLookup[PieceType.Knight] * (pieces[2].Count - pieces[8].Count)
             + pieceValueLookup[PieceType.Bishop] * (pieces[1].Count - pieces[7].Count)
             + pieceValueLookup[PieceType.Pawn] * (pieces[0].Count - pieces[6].Count);
+
+        //----------HEURISTICS----------
+        // castle bonus - avoid states in which we haven't castled! (2 pawns worth)
+        if(board.HasKingsideCastleRight(true) || board.HasQueensideCastleRight(true)) score -= 200; // avoid states in which we still have the right to castle
+        if(board.HasKingsideCastleRight(false) || board.HasQueensideCastleRight(false)) score += 200; // prefer states in which the opponent still has the right to castle
+
+        // bishop pair bonus (worth 1 additional bishop on top of the two)
+        if(pieces[2].Count == 2) score += 350;
+        if(pieces[8].Count == 2) score -= 350;
         
         //----------CHECKMATE----------
         if(board.IsInCheckmate()) score += board.IsWhiteToMove ? double.NegativeInfinity : double.PositiveInfinity;
@@ -60,37 +69,46 @@ public class MyBot : IChessBot
     // iterative version?
     double AlphaBeta(Board board, int depth, double a, double b, bool isMaximizing, bool perspective){
         ulong zobristKey = board.ZobristKey;
-        if(transpTable.ContainsKey(zobristKey)) return transpTable[zobristKey].Item1; // hit in the transposition table! decide if it's worth it to keep searching?
+        if(transpTable.ContainsKey(zobristKey) && transpTable[zobristKey].Item3 != 0) return transpTable[zobristKey].Item1; // hit in the transposition table! if it was a bound (pruned anyways), return that bound
         Move[] moves = board.GetLegalMoves();
         if(depth == 0 || moves.Length == 0){
             double score = Evaluate(board, perspective);
-            transpTable.Add(zobristKey, (score, depth, 0)); // exact value node (leaf node)
+            if(!transpTable.ContainsKey(zobristKey)) transpTable.Add(zobristKey, (score, depth, 0)); // exact value node (leaf node)
             return score;
         }
 
         double value;
+        int bound;
         if(isMaximizing){
             value = double.NegativeInfinity;
+            bound = 0;
             foreach(Move move in moves){
                 board.MakeMove(move);
                 value = Math.Max(value, AlphaBeta(board, depth - 1, a, b, false, perspective)); 
                 board.UndoMove(move);
-                if(value > b) break;
+                if(value > b) {
+                    bound = 1;
+                    break;
+                }
                 a = Math.Max(a, value);
             }
-            if(!transpTable.ContainsKey(zobristKey)) transpTable.Add(zobristKey, (value, depth, 1)); // inner node
+            if(!transpTable.ContainsKey(zobristKey)) transpTable.Add(zobristKey, (value, depth, bound)); // inner node
             return value;
         }
 
         value = double.PositiveInfinity;
+        bound = 0;
         foreach(Move move in moves){
             board.MakeMove(move);
             value = Math.Min(value, AlphaBeta(board, depth - 1, a, b, true, perspective));
             board.UndoMove(move);
-            if(value < a) break;
+            if(value < a) {
+                bound = 1;
+                break;
+            }
             b = Math.Min(b, value);
         }
-        if(!transpTable.ContainsKey(zobristKey)) transpTable.Add(zobristKey, (value, depth, 1)); // inner node
+        if(!transpTable.ContainsKey(zobristKey)) transpTable.Add(zobristKey, (value, depth, bound)); // inner node
         return value;
     }
 }
