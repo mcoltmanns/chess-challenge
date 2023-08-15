3a29d8e4a8ceff0078165e28079bbe376372b794
Max Oltmanns <max.oltmanns@uni-konstanz.de>
2023-08-14 21:37:58 -0400

    lots of new features improved evaluation removed dynamic search depth modification

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -4,35 +4,62 @@ using System;
 
 public class MyBot : IChessBot
 {
-    static Dictionary<PieceType, float> pieceValueLookup = new Dictionary<PieceType, float>() // lookups for piece values
+    Dictionary<PieceType, double> pieceValueLookup = new Dictionary<PieceType, double>() // lookups for piece values
     {
-        {PieceType.None, 0f},
-        {PieceType.Pawn, 1f},
-        {PieceType.Knight, 3f},
-        {PieceType.Bishop, 3f},
-        {PieceType.Rook, 5f},
-        {PieceType.Queen, 9f},
-        {PieceType.King, 200f}
+        {PieceType.None, 0},
+        {PieceType.Pawn, 100},
+        {PieceType.Knight, 325},
+        {PieceType.Bishop, 350},
+        {PieceType.Rook, 525},
+        {PieceType.Queen, 1000},
+        {PieceType.King, 20000}
     };
 
-    static Dictionary<ulong, float> stateInfo = new Dictionary<ulong, float>();
+    // table sources: https://i.stack.imgur.com/hxGdi.png
+    double[] knightValues = {
+        -5, -4, -3, -3, -3, -3, -4, -5,
+        -4, -2, 0, 0, 0, 0, -2, -4,
+        -3, 0, 1, 1.5, 1.5, 1, 0, -3,
+        -3, 0.5, 1.5, 2, 2, 1.5, 0.5, -3,
+        -3, 0.5, 1.5, 2, 2, 1.5, 0.5, -3,
+        -3, 0, 1, 1.5, 1.5, 1, 0, -3,
+        -4, -2, 0, 0, 0, 0, -2, -4,
+        -5, -4, -3, -3, -3, -3, -4, -5
+    };
+
+    double[] pawnValues = {
+        0, 0, 0, 0, 0, 0, 0, 0,
+        5, 5, 5, 5, 5, 5, 5, 5,
+        1, 1, 2, 3, 3, 2, 1, 1,
+        0.5, 0.5, 1, 2.5, 2.5, 1, 0.5, 0.5,
+        0, 0, 0, 2, 2, 0, 0, 0,
+        0.5, -0.5, -1, 0, 0, -1, -0.5, 0.5,
+        0.5, 1, 1, -2, -2, 1, 1, 0.5,
+        0, 0, 0, 0, 0, 0, 0, 0
+    };
+
+    double[] bishopValues = {
+        -2, -1, -1, -1, -1, -1, -1, -2,
+        -1, 0, 0, 0, 0, 0, 0, -1,
+        -1, 0, 0.5, 1, 1, 0.5, 0, -1,
+        -1, 0.5, 0.5, 1, 1, 0.5, 0.5, -1,
+        -1, 0, 1, 1, 1, 1, 0, -1,
+        -1, 1, 1, 1, 1, 1, 1, -1,
+        -1, 0.5, 0, 0, 0, 0, 0.5, -1,
+        -2, -1, -1, -1, -1, -1, -1, -2
+    };
 
     int searchDepth = 2;
 
     public Move Think(Board board, Timer timer)
     {
-        Console.WriteLine("searching to a depth of " + searchDepth + "...");
         Move[] allMoves = board.GetLegalMoves();
-        bool color = board.IsWhiteToMove;
 
-        // Pick a random move to play if nothing better is found
-        Random rng = new();
-        Move moveToPlay = allMoves[rng.Next(allMoves.Length)];
-
-        float bestOutcome = float.NegativeInfinity;
+        Move moveToPlay = Move.NullMove;
+        double bestOutcome = double.NegativeInfinity;
         foreach(Move m in allMoves){
             board.MakeMove(m);
-            float alphaBeta = AlphaBeta(board, searchDepth, float.NegativeInfinity, float.PositiveInfinity, false, color);
+            double alphaBeta = AlphaBeta(board, searchDepth, double.NegativeInfinity, double.PositiveInfinity, false, !board.IsWhiteToMove);
             if(alphaBeta > bestOutcome){
                 bestOutcome = alphaBeta;
                 moveToPlay = m;
@@ -41,129 +68,125 @@ public class MyBot : IChessBot
         }
 
         // deep searches are less important with fewer pieces on the board
+        // deep searches are probably more important in the endgame too (when fewer pieces are on the board)
+        /*
+        int searchLimit = 3;
         // usually there are 40 moves per game, so should take about 1.5 sec per move
-        if(timer.MillisecondsElapsedThisTurn < 1250) searchDepth ++; // if we were fast, go deeper next round - constant is arbitrary, tweak as needed
-        else searchDepth --; // otherwise go shallower
-
-        // i think we need some more heuristics
-
-        Console.WriteLine("found best move " + bestOutcome + " in " + timer.MillisecondsElapsedThisTurn + "ms");
+        if(timer.MillisecondsElapsedThisTurn < 500 && searchDepth < searchLimit) searchDepth ++; // if we were fast, go deeper next round - constant is arbitrary, tweak as needed (maximum depth of 3!)
+        else if(timer.MillisecondsElapsedThisTurn > 1000) searchDepth --; // otherwise go shallower*/
 
         return moveToPlay;
     }
 
     // evaluate with negamax - scored relative to side specified
-    float Evaluate(Board board, bool perspective){
-        float score = 0f;
+    double Evaluate(Board board, bool perspective){
+        double score = 0;
         // all evaluations in here are done from white's side, with positive good/negative bad. then depending on perspective, multiply by -1
         // piece lists
         PieceList[] pieces = board.GetAllPieceLists(); // wp(0), wkn(1), wb(2), wr(3), wq(4), wk(5), bp(6), bkn(7), bb(8), br(9), bq(10), bk(11)
 
-        // consider material
-        float material = pieceValueLookup[PieceType.King] * (pieces[5].Count - pieces[11].Count)
+        //-----------MATERIAL----------
+        double material = pieceValueLookup[PieceType.King] * (pieces[5].Count - pieces[11].Count)
             + pieceValueLookup[PieceType.Queen] * (pieces[4].Count - pieces[10].Count)
             + pieceValueLookup[PieceType.Rook] * (pieces[3].Count - pieces[9].Count)
             + pieceValueLookup[PieceType.Knight] * (pieces[2].Count - pieces[8].Count)
             + pieceValueLookup[PieceType.Bishop] * (pieces[1].Count - pieces[7].Count)
             + pieceValueLookup[PieceType.Pawn] * (pieces[0].Count - pieces[6].Count);
-        score += material * 1f; // weight all material
+        score += material * 1; // weight all material
 
-        // consider mobility
+        //----------HEURISTICS----------
+        // castle bonus - avoid states in which we haven't castled! (2 pawns worth)
+        if(board.HasKingsideCastleRight(true) || board.HasQueensideCastleRight(true)) score -= 200; // avoid states in which we still have the right to castle
+        if(board.HasKingsideCastleRight(false) || board.HasQueensideCastleRight(false)) score += 200; // prefer states in which the opponent still has the right to castle
+
+        // bishop pair bonus (worth 1 additional bishop on top of the two)
+        if(pieces[2].Count == 2) score += 350;
+        if(pieces[8].Count == 2) score -= 350;
+
+        //----------MOBILITY----------
         // figure out which squares each side can move to
         ulong whiteMovesBb = 0;
         ulong blackMovesBb = 0;
-        // need king (5/11)
-        // need knight(2/8)
-        // need pawns(0/6)
-        // need sliders(4/10, 3/9, 1/7)
-        /*
-        num squares white can attack - num squares black can attack
-        */
-        for(int i = 0; i < 6; i++){
+        double pSqVals = 0;
+        for(int i = 0; i < 6; i++){ // piece by piece processing done in this block
             PieceList white = pieces[i];
             PieceList black = pieces[i + 6];
-
-            for(int j = 0; j < white.Count; j++){
-                Piece p = white[j];
-                switch(p.PieceType){
-                    case PieceType.King:
-                        whiteMovesBb |= BitboardHelper.GetKingAttacks(p.Square);
-                        break;
-                    case PieceType.Knight:
-                        whiteMovesBb |= BitboardHelper.GetKnightAttacks(p.Square);
-                        break;
-                    case PieceType.Pawn:
-                        whiteMovesBb |= BitboardHelper.GetPawnAttacks(p.Square, true);
-                        break;
-                    default: // sliders
-                        whiteMovesBb |= BitboardHelper.GetSliderAttacks(p.PieceType, p.Square, board);
-                        break;
-                }
-            }
-
-            for(int j = 0; j < black.Count; j++){
-                Piece p = black[j];
-                switch(p.PieceType){
-                    case PieceType.King:
-                        blackMovesBb |= BitboardHelper.GetKingAttacks(p.Square);
-                        break;
-                    case PieceType.Knight:
-                        blackMovesBb |= BitboardHelper.GetKnightAttacks(p.Square);
-                        break;
-                    case PieceType.Pawn:
-                        blackMovesBb |= BitboardHelper.GetPawnAttacks(p.Square, false);
-                        break;
-                    default: // sliders
-                        blackMovesBb |= BitboardHelper.GetSliderAttacks(p.PieceType, p.Square, board);
-                        break;
-                }
-            }
+            (ulong, double) whiteInfo = EvaluateMobilityAndPieceSquareVals(board, white);
+            (ulong, double) blackInfo = EvaluateMobilityAndPieceSquareVals(board, black);
+            whiteMovesBb |= whiteInfo.Item1;
+            pSqVals += whiteInfo.Item2;
+            blackMovesBb |= blackInfo.Item1;
+            pSqVals -= blackInfo.Item2;
         }
-        score += (BitboardHelper.GetNumberOfSetBits(whiteMovesBb) - BitboardHelper.GetNumberOfSetBits(blackMovesBb)) * 0.1f; // weighted mobility addition - mobility is good to develop early
 
-        // capture potential
-        float captureScore = 0f;
-        foreach(Move m in board.GetLegalMoves(true)){
-            captureScore += pieceValueLookup[m.CapturePieceType];
-        }
-        score += (board.IsWhiteToMove ? captureScore : -captureScore) * 1.5f;// + (board.PlyCount * 0.1f); // captures are more important later on
+        //----------CAPTURE----------
+        // what potential captures can we make from this position
+        //double captureScore = 0;
+        //foreach(Move m in board.GetLegalMoves(true)) captureScore += pieceValueLookup[m.CapturePieceType];
 
-        // check/mate
-        if(board.IsInCheckmate()){
-            score += board.IsWhiteToMove ? float.NegativeInfinity : float.PositiveInfinity;
-        }
+        //----------WEIGHTED SUM----------
+        score += /*(board.IsWhiteToMove ? captureScore : -captureScore) * 0.1 // captures are more important later on
+                +*/ (BitboardHelper.GetNumberOfSetBits(whiteMovesBb) - BitboardHelper.GetNumberOfSetBits(blackMovesBb)) * 0.2 // raw mobility
+                + pSqVals * 10 // piece square values
+                + material; // material
+        
+        //----------CHECKMATE----------
+        if(board.IsInCheckmate()) score += board.IsWhiteToMove ? double.NegativeInfinity : double.PositiveInfinity;
 
         return perspective ? score : -score; // positive good, negative bad!
     }
 
     // iterative version?
-    float AlphaBeta(Board board, int depth, float a, float b, bool isMaximizing, bool perspective){
+    double AlphaBeta(Board board, int depth, double a, double b, bool isMaximizing, bool perspective){
         Move[] moves = board.GetLegalMoves();
         if(depth == 0 || moves.Length == 0){
             return Evaluate(board, perspective);
         }
-        float value;
+        double value;
         if(isMaximizing){
-            value = float.NegativeInfinity;
+            value = double.NegativeInfinity;
             foreach(Move move in moves){
                 board.MakeMove(move);
-                value = MathF.Max(value, AlphaBeta(board, depth - 1, a, b, false, perspective)); 
+                value = Math.Max(value, AlphaBeta(board, depth - 1, a, b, false, perspective)); 
                 board.UndoMove(move);
                 if(value > b) break;
-                a = MathF.Max(a, value);
+                a = Math.Max(a, value);
             }
             return value;
         }
         else{
-            value = float.PositiveInfinity;
+            value = double.PositiveInfinity;
             foreach(Move move in moves){
                 board.MakeMove(move);
-                value = MathF.Min(value, AlphaBeta(board, depth - 1, a, b, true, perspective));
+                value = Math.Min(value, AlphaBeta(board, depth - 1, a, b, true, perspective));
                 board.UndoMove(move);
                 if(value < a) break;
-                b = MathF.Min(b, value);
+                b = Math.Min(b, value);
             }
             return value;
         }
+    }
+
+    // ulong is bitboard of squares reachable by all pieces in piecelist
+    // double is total piece square value
+    (ulong, double) EvaluateMobilityAndPieceSquareVals(Board board, PieceList pieces){
+        ulong movesBb = 0;
+        double pSqVals = 0;
+        for(int j = 0; j < pieces.Count; j++){
+            Piece p = pieces[j];
+            movesBb |= BitboardHelper.GetPieceAttacks(p.PieceType, p.Square, board, true); // raw mobility
+            switch(p.PieceType){
+                case PieceType.Knight: // knight piece square
+                    pSqVals += knightValues[p.Square.Index];
+                    break;
+                case PieceType.Pawn:
+                    pSqVals += pawnValues[p.Square.Index];
+                    break;
+                case PieceType.Bishop:
+                    pSqVals += bishopValues[p.Square.Index];
+                    break;
+            }
+        }
+        return (movesBb, pSqVals);
     }
 }
